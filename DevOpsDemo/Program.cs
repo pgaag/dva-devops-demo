a6e7a3ab1dd6bce2bd1d50871ab2c18b40299f20
pgaag <40818876+pgaag@users.noreply.github.com>
2022-04-15 15:56:40 +0200

    Update Program.cs

--- DevOpsDemo/Program.cs
+++ DevOpsDemo/Program.cs
@@ -3,7 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
