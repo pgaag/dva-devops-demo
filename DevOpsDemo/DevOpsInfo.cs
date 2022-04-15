98bfd8eeae459d48ffc729242e9a1ae8e075dfeb
philipp.gaag@hs-augsburg.de <1kC1IdZDehOLqdu4qXEc>
2022-04-15 17:01:19 +0200

    removed warnings

--- DevOpsDemo/DevOpsInfo.cs
+++ DevOpsDemo/DevOpsInfo.cs
@@ -2,9 +2,9 @@
 
 public class DevOpsInfo
 {
-    public string[] Pratices { get; set; }
+    public string[]? Pratices { get; set; }
     
-    public string[] Contributers { get; set; }
+    public string[]? Contributers { get; set; }
 
-    public string ClassName { get; set; }
+    public string? ClassName { get; set; }
 }
