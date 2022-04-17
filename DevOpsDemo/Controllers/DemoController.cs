8e1283f5c46bd7ac78f1cc34e72de44d8b037468
philipp.gaag@hs-augsburg.de <1kC1IdZDehOLqdu4qXEc>
2022-04-17 21:44:54 +0200

    added demo controller

---
+++ DevOpsDemo/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using DevOpsDemo.Data;
+using DevOpsDemo.Model;
+using DevOpsDemo.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevOpsDemo.Controllers;
+
+public class DemoController : ControllerBase
+{
+    private readonly ILogger<DevOpsController>? _logger;
+
+    public DemoController(ILogger<DevOpsController>? logger)
+    {
+        _logger = Validate.NotNull(() => logger);
+    }
+    
+    [HttpGet("info")]
+    public IActionResult GetDevOpsInfo()
+    {
+        return Ok(new DevOpsInfo()
+        {
+            Contributers = Database.Contributors,
+            Practices = Database.Practices,
+            ClassName = Database.ClassName,
+            ProjectState = State.Ongoing
+        });
+    }
+}
