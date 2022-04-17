b611dc45998441ca27f2bf525175667819a14125
philipp.gaag@hs-augsburg.de <1kC1IdZDehOLqdu4qXEc>
2022-04-17 21:35:59 +0200

    removed some tests

--- DevOpsDemo.Tests/DevOpsControllerTests.cs
+++ DevOpsDemo.Tests/DevOpsControllerTests.cs
@@ -78,77 +78,76 @@ public class DevOpsControllerTests
         Assert.Equal(404, resultObj?.StatusCode);
     }
     
-    [Fact]
-    public void Practices_Returns_Not_Found_When_Null()
-    {
-        // Arrange
-        var mockLogger = new Mock<ILogger<DevOpsController>>();
-        var controller = new DevOpsController(mockLogger.Object, false);
-
-        // Act
-        var result = controller.GetPractices();
-        var resultObj = result as StatusCodeResult;
-        
-        // Assert
-        Assert.Equal(404, resultObj?.StatusCode);
-    }
-    
-    [Fact]
-    public void Practices_Are_Returned_Correctly()
-    {
-        // Arrange
-        var mockLogger = new Mock<ILogger<DevOpsController>>();
-        var controller = new DevOpsController(mockLogger.Object);
-        
-        // Act
-        var result = controller.GetPractices();
-        var resultObj = result as OkObjectResult;
-        
-        // Assert
-        Assert.NotNull(resultObj?.Value);
-        Assert.Equal(200, resultObj.StatusCode);
-    }
-    
-    [Fact]
-    public void DevOpsInfo_Is_Returned_Correctly()
-    {
-        // Arrange
-        var mockLogger = new Mock<ILogger<DevOpsController>>();
-        var controller = new DevOpsController(mockLogger.Object);
-        
-        // Act
-        var result = controller.GetDevOpsInfo();
-        var resultObj = result as OkObjectResult;
-        
-        // Assert
-        Assert.NotNull(resultObj?.Value);
-        Assert.IsType<DevOpsInfo>(resultObj.Value);
-        var devOpsInfo = (DevOpsInfo)resultObj.Value;
-        Assert.NotNull(devOpsInfo.Contributers);
-        Assert.NotNull(devOpsInfo.Practices);
-        Assert.NotNull(devOpsInfo.ClassName);
-        Assert.Equal(200, resultObj.StatusCode);
-    }
-    
-    [Fact]
-    public void DevOpsInfo_Return_Not_Found_If_Null()
-    {
-        // Arrange
-        var mockLogger = new Mock<ILogger<DevOpsController>>();
-        var controller = new DevOpsController(mockLogger.Object, false);
-        
-        // Act
-        var result = controller.GetDevOpsInfo();
-        var resultObj = result as OkObjectResult;
-        
-        // Assert
-        Assert.NotNull(resultObj?.Value);
-        Assert.IsType<DevOpsInfo>(resultObj.Value);
-        var devOpsInfo = (DevOpsInfo)resultObj.Value;
-        Assert.Null(devOpsInfo.Contributers);
-        Assert.Null(devOpsInfo.Practices);
-        Assert.Null(devOpsInfo.ClassName);
-        Assert.Equal(200, resultObj.StatusCode);
-    }
-    
+    // [Fact]
+    // public void Practices_Returns_Not_Found_When_Null()
+    // {
+    //     // Arrange
+    //     var mockLogger = new Mock<ILogger<DevOpsController>>();
+    //     var controller = new DevOpsController(mockLogger.Object, false);
+    //
+    //     // Act
+    //     var result = controller.GetPractices();
+    //     var resultObj = result as StatusCodeResult;
+    //     
+    //     // Assert
+    //     Assert.Equal(404, resultObj?.StatusCode);
+    // }
+    //
+    // [Fact]
+    // public void Practices_Are_Returned_Correctly()
+    // {
+    //     // Arrange
+    //     var mockLogger = new Mock<ILogger<DevOpsController>>();
+    //     var controller = new DevOpsController(mockLogger.Object);
+    //     
+    //     // Act
+    //     var result = controller.GetPractices();
+    //     var resultObj = result as OkObjectResult;
+    //     
+    //     // Assert
+    //     Assert.NotNull(resultObj?.Value);
+    //     Assert.Equal(200, resultObj.StatusCode);
+    // }
+    //
+    // [Fact]
+    // public void DevOpsInfo_Is_Returned_Correctly()
+    // {
+    //     // Arrange
+    //     var mockLogger = new Mock<ILogger<DevOpsController>>();
+    //     var controller = new DevOpsController(mockLogger.Object);
+    //     
+    //     // Act
+    //     var result = controller.GetDevOpsInfo();
+    //     var resultObj = result as OkObjectResult;
+    //     
+    //     // Assert
+    //     Assert.NotNull(resultObj?.Value);
+    //     Assert.IsType<DevOpsInfo>(resultObj.Value);
+    //     var devOpsInfo = (DevOpsInfo)resultObj.Value;
+    //     Assert.NotNull(devOpsInfo.Contributers);
+    //     Assert.NotNull(devOpsInfo.Practices);
+    //     Assert.NotNull(devOpsInfo.ClassName);
+    //     Assert.Equal(200, resultObj.StatusCode);
+    // }
+    //
+    // [Fact]
+    // public void DevOpsInfo_Return_Not_Found_If_Null()
+    // {
+    //     // Arrange
+    //     var mockLogger = new Mock<ILogger<DevOpsController>>();
+    //     var controller = new DevOpsController(mockLogger.Object, false);
+    //     
+    //     // Act
+    //     var result = controller.GetDevOpsInfo();
+    //     var resultObj = result as OkObjectResult;
+    //     
+    //     // Assert
+    //     Assert.NotNull(resultObj?.Value);
+    //     Assert.IsType<DevOpsInfo>(resultObj.Value);
+    //     var devOpsInfo = (DevOpsInfo)resultObj.Value;
+    //     Assert.Null(devOpsInfo.Contributers);
+    //     Assert.Null(devOpsInfo.Practices);
+    //     Assert.Null(devOpsInfo.ClassName);
+    //     Assert.Equal(200, resultObj.StatusCode);
+    // }
 }
