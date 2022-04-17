984864de510be5ebff8f43a818b30c87ef88d879
philipp.gaag@hs-augsburg.de <1kC1IdZDehOLqdu4qXEc>
2022-04-17 16:42:53 +0200

    added a test

--- DevOpsDemo.Tests/DevOpsControllerTests.cs
+++ DevOpsDemo.Tests/DevOpsControllerTests.cs
@@ -1,4 +1,6 @@
 ﻿using DevOpsDemo.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace DevOpsDemo.Tests;
@@ -9,5 +11,23 @@ public class DevOpsControllerTests
     public void Constructor_Should_Throw_If_Logger_Is_Null()
     {
         Assert.Throws<ArgumentNullException>(() => new DevOpsController(null));
+    }
+    
+    [Fact]
+    public void Contributers_Are_Returned_Correctly()
+    {
+        // Arrange
+        var mockLogger = new Mock<ILogger<DevOpsController>>();
+        var controller = new DevOpsController(mockLogger.Object);
+        
+        // Act
+        var result = controller.GetContributers();
+        
+        // Assert
+        string[] Contributers = 
+        {
+            "Philipp", "Marcel", "Marco", "Fabian", "Daniel", "Roman"
+        };
+        Assert.Equal(result, Contributers);
     }
 }
