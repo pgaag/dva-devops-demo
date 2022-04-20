f73fc8316053640c023abba38ded529b88ee45de
philipp.gaag@hs-augsburg.de <1kC1IdZDehOLqdu4qXEc>
2022-04-20 21:43:49 +0200

    readded some tests

--- DevOpsDemo.Tests/DevOpsControllerTests.cs
+++ DevOpsDemo.Tests/DevOpsControllerTests.cs
@@ -32,65 +32,65 @@ public class DevOpsControllerTests
         Assert.Equal(200, resultObj.StatusCode);
     }
     
-    // [Fact]
-    // public void Contributors_Returns_Not_Found_When_Null()
-    // {
-    //     // Arrange
-    //     var mockLogger = new Mock<ILogger<DevOpsController>>();
-    //     var controller = new DevOpsController(mockLogger.Object, false);
+    [Fact]
+    public void Contributors_Returns_Not_Found_When_Null()
+    {
+        // Arrange
+        var mockLogger = new Mock<ILogger<DevOpsController>>();
+        var controller = new DevOpsController(mockLogger.Object, false);
 
-    //     // Act
-    //     var result = controller.GetContributors();
-    //     var resultObj = result as StatusCodeResult;
+        // Act
+        var result = controller.GetContributors();
+        var resultObj = result as StatusCodeResult;
         
-    //     // Assert
-    //     Assert.Equal(404, resultObj?.StatusCode);
-    // }
+        // Assert
+        Assert.Equal(404, resultObj?.StatusCode);
+    }
     
-    // [Fact]
-    // public void DVA_Class_Is_Returned_Correctly()
-    // {
-    //     // Arrange
-    //     var mockLogger = new Mock<ILogger<DevOpsController>>();
-    //     var controller = new DevOpsController(mockLogger.Object);
+    [Fact]
+    public void DVA_Class_Is_Returned_Correctly()
+    {
+        // Arrange
+        var mockLogger = new Mock<ILogger<DevOpsController>>();
+        var controller = new DevOpsController(mockLogger.Object);
         
-    //     // Act
-    //     var result = controller.GetClass();
-    //     var resultObj = result as OkObjectResult;
+        // Act
+        var result = controller.GetClass();
+        var resultObj = result as OkObjectResult;
         
-    //     // Assert
-    //     Assert.NotNull(resultObj?.Value);
-    //     Assert.Equal(200, resultObj.StatusCode);
-    // }
+        // Assert
+        Assert.NotNull(resultObj?.Value);
+        Assert.Equal(200, resultObj.StatusCode);
+    }
     
-    // [Fact]
-    // public void DVA_Class_Returns_Not_Found_When_Null()
-    // {
-    //     // Arrange
-    //     var mockLogger = new Mock<ILogger<DevOpsController>>();
-    //     var controller = new DevOpsController(mockLogger.Object, false);
+    [Fact]
+    public void DVA_Class_Returns_Not_Found_When_Null()
+    {
+        // Arrange
+        var mockLogger = new Mock<ILogger<DevOpsController>>();
+        var controller = new DevOpsController(mockLogger.Object, false);
         
-    //     // Act
-    //     var result = controller.GetClass();
-    //     var resultObj = result as StatusCodeResult;
+        // Act
+        var result = controller.GetClass();
+        var resultObj = result as StatusCodeResult;
         
-    //     // Assert
-    //     Assert.Equal(404, resultObj?.StatusCode);
-    // }
+        // Assert
+        Assert.Equal(404, resultObj?.StatusCode);
+    }
     
-    // [Fact]
-    // public void Practices_Returns_Not_Found_When_Null()
-    // {
-    //     // Arrange
-    //     var mockLogger = new Mock<ILogger<DevOpsController>>();
-    //     var controller = new DevOpsController(mockLogger.Object, false);
+    [Fact]
+    public void Practices_Returns_Not_Found_When_Null()
+    {
+        // Arrange
+        var mockLogger = new Mock<ILogger<DevOpsController>>();
+        var controller = new DevOpsController(mockLogger.Object, false);
 
-    //     // Act
-    //     var result = controller.GetPractices();
-    //     var resultObj = result as StatusCodeResult;
+        // Act
+        var result = controller.GetPractices();
+        var resultObj = result as StatusCodeResult;
         
-    //     // Assert
-    //     Assert.Equal(404, resultObj?.StatusCode);
+        // Assert
+        Assert.Equal(404, resultObj?.StatusCode);
     // }
     
     // [Fact]
