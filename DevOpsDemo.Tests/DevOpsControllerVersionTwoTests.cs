09819b8015920dc2e7a8d8487b009beb6f59fe9c
philipp.gaag@hs-augsburg.de <1kC1IdZDehOLqdu4qXEc>
2022-04-21 09:06:10 +0200

    added some more tests

--- DevOpsDemo.Tests/DevOpsControllerVersionTwoTests.cs
+++ DevOpsDemo.Tests/DevOpsControllerVersionTwoTests.cs
@@ -78,36 +78,36 @@ public class DevOpsControllerVersionTwoTests
         Assert.Equal(404, resultObj?.StatusCode);
     }
     
-    // [Fact]
-    // public void Practices_Returns_Not_Found_When_Null()
-    // {
-    //     // Arrange
-    //     var mockLogger = new Mock<ILogger<DevOpsController>>();
-    //     var controller = new DevOpsController(mockLogger.Object, false);
-    //
-    //     // Act
-    //     var result = controller.GetPractices();
-    //     var resultObj = result as StatusCodeResult;
-    //     
-    //     // Assert
-    //     Assert.Equal(404, resultObj?.StatusCode);
-    // }
-    //
-    // [Fact]
-    // public void Practices_Are_Returned_Correctly()
-    // {
-    //     // Arrange 
-    //     var mockLogger = new Mock<ILogger<DevOpsControllerVersionTwo>>();
-    //     var controller = new DevOpsControllerVersionTwo(mockLogger.Object);
-    //     
-    //     // Act
-    //     var result = controller.GetPractices();
-    //     var resultObj = result as OkObjectResult;
-    //     
-    //     // Assert
-    //     Assert.NotNull(resultObj?.Value);
-    //     Assert.Equal(200, resultObj.StatusCode);
-    // }
+    [Fact]
+    public void Practices_Returns_Not_Found_When_Null()
+    {
+        // Arrange
+        var mockLogger = new Mock<ILogger<DevOpsController>>();
+        var controller = new DevOpsController(mockLogger.Object, false);
+    
+        // Act
+        var result = controller.GetPractices();
+        var resultObj = result as StatusCodeResult;
+        
+        // Assert
+        Assert.Equal(404, resultObj?.StatusCode);
+    }
+    
+    [Fact]
+    public void Practices_Are_Returned_Correctly()
+    {
+        // Arrange 
+        var mockLogger = new Mock<ILogger<DevOpsControllerVersionTwo>>();
+        var controller = new DevOpsControllerVersionTwo(mockLogger.Object);
+        
+        // Act
+        var result = controller.GetPractices();
+        var resultObj = result as OkObjectResult;
+        
+        // Assert
+        Assert.NotNull(resultObj?.Value);
+        Assert.Equal(200, resultObj.StatusCode);
+    }
     //
     // [Fact]
     // public void DevOpsInfo_Is_Returned_Correctly()
